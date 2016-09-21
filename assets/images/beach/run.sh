b997b94be4a213da2e2000a11e3fb8b66f5af5fb
Zack Scholl <zack.scholl@gmail.com>
2016-09-21 12:57:36 -0400

    Uploaded images

---
+++ assets/images/beach/run.sh
@@ -0,0 +1,4 @@
+$GOPATH/bin/primitive -i original.jpg -o 30.png -n 30 -m 5 -r 256 -s 1024 -a 255 -v
+$GOPATH/bin/primitive -i original.jpg -o 40.png -n 40 -m 5 -r 256 -s 1024 -a 255 -v
+$GOPATH/bin/primitive -i original.jpg -o 60.png -n 60 -m 5 -r 256 -s 1024 -a 255 -v
+$GOPATH/bin/primitive -i original.jpg -o 120.png -n 120 -m 5 -r 256 -s 1024 -a 255 -v
